1ec7688f6a03cf1f12cdb0b4a22d4dd5ca1f28cf
Evan Voeltner <evanvoeltner@gmail.com>
2022-09-28 10:46:10 -0500

    Added all CRUD functionality to supers + made the urls useable

--- supers/views.py
+++ supers/views.py
@@ -1,3 +1,36 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
+from .serializers import SuperSerializer
+from .models import Super
 
-# Create your views here.
+
+@api_view(['GET', 'POST'])
+def supers_list(request):
+    
+    if request.method == 'GET':
+        products = Super.objects.all()
+        serializer = SuperSerializer(products, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+    elif request.method == 'POST':
+        serializer =  SuperSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+@api_view(['GET', 'PUT', 'DELETE'])
+def super_detail(request, pk):
+    product = get_object_or_404(Super, pk=pk)
+    if request.method == 'GET':
+        serializer = SuperSerializer(product)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+    elif request.method == 'PUT':
+        serializer = SuperSerializer(product, data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_200_OK)
+    elif request.method == 'DELETE':
+        product.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
